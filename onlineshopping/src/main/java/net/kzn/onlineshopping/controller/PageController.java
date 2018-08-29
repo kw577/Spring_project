8d9ff07bb6747bf899b26242f977d4f2c2ca62b3
K W <kw577@wp.pl>
2018-08-29 11:31:46 +0200

    Frontend - test -  RequestMapping - zapytania do projektu frontendowego za pomoca adresu html

--- onlineshopping/src/main/java/net/kzn/onlineshopping/controller/PageController.java
+++ onlineshopping/src/main/java/net/kzn/onlineshopping/controller/PageController.java
@@ -2,13 +2,14 @@ package net.kzn.onlineshopping.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.ModelAndView;
 
 @Controller
 public class PageController {
 
-	
-	@RequestMapping(value = {"/", "/home", "/index"})
+	// funkcja zwracajaca ModelAndView - zawartosc strony 
+	@RequestMapping(value = {"/", "/home", "/index"}) // adres html np: http://localhost:8080/onlineshopping/home
 	public ModelAndView index() {
 		
 		ModelAndView mv = new ModelAndView("page");
@@ -18,5 +19,21 @@ public class PageController {
 		
 		
 	}
+	
+	
+	// greeting to parametr zapytania html - w zaleznosci od jego wartosci zostanie wyswietlona inna zawartosc 
+	@RequestMapping(value = {"/test"}) // adres html: http://localhost:8080/onlineshopping/test
+	public ModelAndView test(@RequestParam(value = "greeting", required = false) String greeting) { // required = false - adres html nie musi koniecznie zawierac parametru greeting
+		
+		// jesli w adresie html nie podano parametru greeting
+		if(greeting == null) {
+			greeting = "Hello";
+		}
+		
+		ModelAndView mv = new ModelAndView("page");
+		mv.addObject("greeting",greeting);
+		return mv;
+	}
+	
 	
 }
