3c8936eec4af040e5aa052f8559cfa49bd6fcb60
K W <kw577@wp.pl>
2018-08-29 11:46:48 +0200

    Frontend - test -  PathVariable - zapytania do projektu frontendowego za pomoca adresu html - dynamiczne

--- onlineshopping/src/main/java/net/kzn/onlineshopping/controller/PageController.java
+++ onlineshopping/src/main/java/net/kzn/onlineshopping/controller/PageController.java
@@ -1,8 +1,8 @@
 package net.kzn.onlineshopping.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.ModelAndView;
 
 @Controller
@@ -20,9 +20,10 @@ public class PageController {
 		
 	}
 	
-	
+	/*
+	// funkcja ponizej napisana do celow testowych
 	// greeting to parametr zapytania html - w zaleznosci od jego wartosci zostanie wyswietlona inna zawartosc 
-	@RequestMapping(value = {"/test"}) // adres html: http://localhost:8080/onlineshopping/test
+	@RequestMapping(value = {"/test"}) // adres html: http://localhost:8080/onlineshopping/test  adres html: http://localhost:8080/onlineshopping/test?greeting=good morning
 	public ModelAndView test(@RequestParam(value = "greeting", required = false) String greeting) { // required = false - adres html nie musi koniecznie zawierac parametru greeting
 		
 		// jesli w adresie html nie podano parametru greeting
@@ -34,6 +35,27 @@ public class PageController {
 		mv.addObject("greeting",greeting);
 		return mv;
 	}
+	*/
+	// funkcja testowa
+	// funkcja z parametrem dynamicznym
+	// greeting to parametr zapytania html - w zaleznosci od jego wartosci zostanie wyswietlona inna zawartosc 
+		@RequestMapping(value = {"/test/{greeting}"}) // adres html: http://localhost:8080/onlineshopping/test/trescPwitania
+		public ModelAndView test(@PathVariable("greeting") String greeting) { 
+			
+			// jesli w adresie html nie podano parametru greeting
+			if(greeting == null) {
+				greeting = "Hello";
+			}
+			
+			ModelAndView mv = new ModelAndView("page");
+			mv.addObject("greeting",greeting);
+			return mv;
+		}
+	
+	
+	
+	
+	
 	
 	
 }
