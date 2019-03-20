5ed060b2fb043aeabc4230b41e3c0cd725ba88e4
Krzysztof Wieckowski <krzysztof.wieckowski@ericsson.com>
2019-03-20 13:42:24 +0100

    Spring Security - role based access control - rozny dostep do zasobow stronydla uzytkownika i admina. Dodanie strony z informacja o braku dostepu do okreslonych zasobow

--- onlineshopping/src/main/java/net/kzn/onlineshopping/controller/PageController.java
+++ onlineshopping/src/main/java/net/kzn/onlineshopping/controller/PageController.java
@@ -213,4 +213,20 @@ public class PageController {
 	}
 	
 	
+	
+	// access denied page
+	@RequestMapping(value = { "/access-denied" }) 
+	public ModelAndView accessDenied() {
+
+		ModelAndView mv = new ModelAndView("error");
+	
+		mv.addObject("title", "403 - Access Denied");
+		mv.addObject("errorTitle", "Error");
+		mv.addObject("errorDescription", "You are not authorized to view this page!");
+		
+		return mv;
+
+	}
+	
+	
 }
